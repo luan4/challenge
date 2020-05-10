9c0b5bac62a701df97b3fd14d5d217d3c8a30a1e
luan4 <julian.guagnino@gmail.com>
2020-05-09 23:54:19 -0300

    removed old flaskr directory

--- flaskr/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-user = "test"
-password = "password"
-host = "localhost"
-database = "example"
-port = "5432"
-
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
